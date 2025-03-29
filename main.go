86628c85963464ccd31156e63a19c61d708c60c6
Fabio Cuciuffo <cuciuffofab@gmail.com>
2025-03-29 01:08:19 +0000

    ajout de la possibilité de spécifier un path et de print l'utilisation mémoire du programme via le flag print-mem + refacto du contains utilisation du built in

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
+	"runtime"
+	"slices"
 	"time"
 )
 
@@ -23,38 +26,58 @@ var TOTAL_SIZE float64 = 0
 var TIME_ELPASED float64 = 0
 
 func main() {
-	dir, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
+	var dir string
+	args := os.Args[1:2]
+
+	if string(args[0][0]) == "-" {
+		dir, _ = os.Getwd()
+	} else {
+		dir = args[0]
+
 	}
-	fsys := os.DirFS(dir)
+
+	exec(dir)
+	printExecLogs()
+	printMem := flag.Bool("print-mem", false, "bool print memory")
+	flag.Parse()
+	if *printMem {
+		printMemUsage()
+	}
+}
+
+func exec(basePath string) {
+	fsys := os.DirFS(basePath)
 	fs.WalkDir(fsys, ".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			log.Fatal(err)
+			os.Exit(1)
 		}
 		if !d.IsDir() {
 			return nil
 		}
 
-		if !contains(DIRECTORIES_TO_DELETE, d.Name()) {
+		if !slices.Contains(DIRECTORIES_TO_DELETE, d.Name()) {
 			return nil
+		}
+
+		if string(basePath[len(basePath)-1]) == "/" {
+			path = basePath + path
+		} else {
+			path = basePath + "/" + path
 		}
 
 		currentSize, err := calculateTotalSize(path)
 		if err != nil {
 			log.Fatal(err)
+			os.Exit(1)
 		}
+
 		TOTAL_SIZE += currentSize
 
 		err = os.RemoveAll(path)
 		if err != nil {
 			log.Fatal(err)
-		}
-
-		err = os.RemoveAll(path)
-
-		if err != nil {
-			log.Fatal(err)
+			os.Exit(1)
 		}
 
 		NO_DELETE = false
@@ -63,22 +86,15 @@ func main() {
 
 		return fs.SkipDir
 	})
+}
 
+func printExecLogs() {
 	if NO_DELETE {
 		fmt.Println("\033[33mAucun répertoire supprimé.\033[0m")
 	} else {
 		TIME_ELPASED = float64(time.Now().UnixMilli()-TIME_START) / 1000
-		fmt.Printf("\033[32mEspace libéré %.2fMo en %.3fs\033[0m", TOTAL_SIZE/(1024*1024), TIME_ELPASED)
+		fmt.Printf("\033[32mEspace libéré %.2fMo en %.3fs\033[0m\n", TOTAL_SIZE/(1024*1024), TIME_ELPASED)
 	}
-}
-
-func contains[T comparable](slice []T, val T) bool {
-	for _, item := range slice {
-		if item == val {
-			return true
-		}
-	}
-	return false
 }
 
 func calculateTotalSize(path string) (float64, error) {
@@ -97,4 +113,17 @@ func calculateTotalSize(path string) (float64, error) {
 		return 0, err
 	}
 	return totalSize, nil
+}
+
+func printMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	fmt.Printf("Alloc = %v MiB\n", bToMb(m.Alloc))
+	fmt.Printf("TotalAlloc = %v MiB\n", bToMb(m.TotalAlloc))
+	fmt.Printf("Sys = %v MiB\n", bToMb(m.Sys))
+	fmt.Printf("NumGC = %v\n", m.NumGC)
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
 }
